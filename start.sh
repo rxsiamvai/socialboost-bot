39a68825b4d0fa357a47263347e301f1c9033377
rxsiamvai <siam@gmail.com>
2026-08-17 11:38:40 +0600

    Run web server in background for port binding

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python -c "import telebot; b=telebot.TeleBot('8656424951:AAEFKKgwDdZbGS68SyL68AAqeql_LAp5Nko'); b.remove_webhook()"
+python -c "import keep_alive; keep_alive.keep_alive()" &
 python bot.py
